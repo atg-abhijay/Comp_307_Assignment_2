719360970d0721794b76915d2612c2e10ea1fd2b
atg-abhijay <atg.abhijay@gmail.com>
2017-09-30 19:00:08 -0400

    added transpose method and working on encryption method

--- MyDES.java
+++ MyDES.java
@@ -19,4 +19,56 @@ public class MyDES {
         System.out.println("You entered: " + message);
 
     }
+
+    public static String encrypt(String toEncrypt, int key) {
+        /* creating a new 2D array to hold
+            the user's input string */
+        String[][] T = new String[key/10][1000];
+        char[] stringAsChars = toEncrypt.toCharArray();
+        int charIndex = 0;
+        loop:
+        for (int i = 0; i < T.length; i++) {
+            for (int j = 0; j < T[i].length; j++) {
+                T[i][j] = stringAsChars[charIndex];
+                charIndex++;
+                if (charIndex == stringAsChars.length){
+                    break loop;
+                }
+            }
+        }
+
+        char[] A = new char[toEncrypt.length()]
+        int transposedCharIndex = 0;
+        String[][] transposeOfT = transpose(T);
+        for (int i = 0; i < transposeOfT.length; i++) {
+            for (int j = 0; j < transposeOfT[0].length; j++) {
+                A[transposedCharIndex] = transposeOfT[i][j];
+                transposedCharIndex++;
+            }
+        }
+
+        return "Done";
+    }
+
+    public static String[][] transpose(String[][] T) {
+        String[][] transposeOfT = new String[T[0].length][T.length];
+        int height = 0;
+        int width = 0;
+        for(int i = 0; i < T.length; i++) {
+            for(int j = 0; j < T[0].length; j++) {
+                transposeOfT[height][width] = T[i][j];
+                height++;
+                if(height == transposeOfT.length) {
+                    height = 0;
+                    width++;
+                }
+            }
+        }
+        return transposeOfT;
+    }
+
+    public static String decrypt(String toDecrypt) {
+        return null;
+    }
+
 }
