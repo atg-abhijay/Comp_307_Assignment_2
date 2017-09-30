8178ee99e1a53a4aca447411a7b6126b15ffb0e8
atg-abhijay <atg.abhijay@gmail.com>
2017-09-30 19:50:00 -0400

    breaking into separate methods

--- MyDES.java
+++ MyDES.java
@@ -2,56 +2,92 @@ import java.util.Scanner;
 
 public class MyDES {
     public static void main(String[] args){
-        Scanner reader = new Scanner();
-        String numberMessage = "Please enter a two-digit positive number";
-        System.out.println(numberMessage);
-        int key = reader.nextInt();
-        if (key <= 9 || key >= 100) {
-            System.out.println("The number you have entered is incorrect." + numberMessage);
-            return;
+        // Scanner reader = new Scanner();
+        // String numberMessage = "Please enter a two-digit positive number";
+        // System.out.println(numberMessage);
+        // int key = reader.nextInt();
+        // if (key <= 9 || key >= 100) {
+        //     System.out.println("The number you have entered is incorrect." + numberMessage);
+        //     return;
+        // }
+        // System.out.println("Please enter a single sentence");
+        // String message = reader.next();
+        // if (message.length() == 0 || message.equals("")){
+        //     System.out.println("You have not entered a valid sentence");
+        //     return;
+        // }
+        // System.out.println("You entered: " + message);
+        String foo = "abc";
+        char[] A = foo.toCharArray();
+        char[] B = new char[A.length];
+        for (int i = 0; i < B.length; i++) {
+            int newChar = A[i] + 5;
+            B[i] = (char)newChar;
         }
-        System.out.println("Please enter a single sentence");
-        String message = reader.next();
-        if (message.length() == 0 || message.equals("")){
-            System.out.println("You have not entered a valid sentence");
-            return;
+        for (int i = 0; i < B.length; i++) {
+            System.out.print(B[i]);
         }
-        System.out.println("You entered: " + message);
 
     }
 
-    public static String encrypt(String toEncrypt, int key) {
-        /* creating a new 2D array to hold
-            the user's input string */
-        String[][] T = new String[key/10][1000];
-        char[] stringAsChars = toEncrypt.toCharArray();
+    public static char[][] populateCharArray(char[] stringAsChars, int useKey) {
+        char[][] stringBlock = new char[useKey][1000];
         int charIndex = 0;
         loop:
-        for (int i = 0; i < T.length; i++) {
-            for (int j = 0; j < T[i].length; j++) {
-                T[i][j] = stringAsChars[charIndex];
+        for (int i = 0; i < stringBlock.length; i++) {
+            for (int j = 0; j < stringBlock[i].length; j++) {
+                stringBlock[i][j] = stringAsChars[charIndex];
                 charIndex++;
                 if (charIndex == stringAsChars.length){
                     break loop;
                 }
             }
         }
+        return stringBlock;
+    }
 
-        char[] A = new char[toEncrypt.length()]
-        int transposedCharIndex = 0;
-        String[][] transposeOfT = transpose(T);
-        for (int i = 0; i < transposeOfT.length; i++) {
-            for (int j = 0; j < transposeOfT[0].length; j++) {
-                A[transposedCharIndex] = transposeOfT[i][j];
-                transposedCharIndex++;
+    public static char[] makeCharFromTranspose(char[][] transpose, int arrayLength) {
+        int index = 0;
+        char[] charArray = new char[arrayLength];
+        for (int i = 0; i < transpose.length; i++) {
+            for (int j = 0; j < transpose[0].length; j++) {
+                charArray[index] = transpose[i][j];
+                index++;
             }
         }
+        return charArray;
+    }
+
+
+    public static String encrypt(String toEncrypt, int key) {
+        // populate char[][] with user's input string
+        char[] userStringAsChar = toEncrypt.toCharArray();
+        char[][] T = populateCharArray(userStringAsChar, key/10);
+        // take tranpose of char[][]
+        char[][] transposeOfT = transpose(T);
+        /* making a char[] from the
+            transpose of char[][] T */
+        char[] A = makeCharFromTranspose(transposeOfT, toEncrypt.length());
+        /* making a new char[] B, where
+            B is the Caesar cipher of A */
+        char[] B = new char[A.length];
+        for (int i = 0; i < B.length; i++) {
+            int newChar = A[i] + key;
+            B[i] = (char) newChar;
+        }
+
+        /* making a char[][] using
+            the char[] B */
+        char[][] T2 = populateCharArray(B, (key/10)+1);
+        // taking transpose of T2
+        char[][] transposeT2 = transpose(T2);
+
 
         return "Done";
     }
 
-    public static String[][] transpose(String[][] T) {
-        String[][] transposeOfT = new String[T[0].length][T.length];
+    public static char[][] transpose(char[][] T) {
+        char[][] transposeOfT = new char[T[0].length][T.length];
         int height = 0;
         int width = 0;
         for(int i = 0; i < T.length; i++) {
